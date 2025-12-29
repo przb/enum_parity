a7bc27808dacaa7b9b459c4e1568ce6ab08d85d2
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2025-12-29 17:54:58 -0600

    make bit_parity iter generic

--- src/bit_par_iter.rs
+++ src/bit_par_iter.rs
@@ -1,14 +1,14 @@
-pub(crate) struct BitParityIter {
+pub(crate) struct BitParityIter<T> {
     /// the previous value
     ///
     /// `None` if this is the first iteration, `Some` otherwise
-    prev_val: Option<u64>,
+    prev_val: Option<T>,
 
     /// `true` if there should be an even number of ones, `false` otherwise
     is_even_parity: bool,
 }
 
-impl BitParityIter {
+impl<T> BitParityIter<T> {
     pub(crate) fn new(is_even: bool) -> Self {
         Self {
             prev_val: None,
@@ -17,25 +17,33 @@ impl BitParityIter {
     }
 }
 
-impl Iterator for BitParityIter {
-    type Item = u64;
+macro_rules! bit_par_iter_impl {
+    ($($int:ty),*) => {
+        $(impl Iterator for BitParityIter<$int> {
+            type Item = $int;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let val = if let Some(mut val) = self.prev_val {
-            val = val.checked_add(1)?;
-            while val.count_ones().is_multiple_of(2) != self.is_even_parity {
-                val = val.checked_add(1)?;
+            fn next(&mut self) -> Option<Self::Item> {
+                let val = if let Some(mut val) = self.prev_val {
+                    val = val.checked_add(1)?;
+                    while val.count_ones().is_multiple_of(2) != self.is_even_parity {
+                        val = val.checked_add(1)?;
+                    }
+                    val
+                } else {
+                    if self.is_even_parity { 0 } else { 1 }
+                };
+
+                self.prev_val = Some(val);
+
+                Some(val)
             }
-            val
-        } else {
-            if self.is_even_parity { 0 } else { 1 }
-        };
-
-        self.prev_val = Some(val);
-
-        Some(val)
-    }
+        })*
+    };
 }
+
+bit_par_iter_impl!(
+    u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize
+);
 
 #[cfg(test)]
 mod tests {
@@ -44,7 +52,7 @@ mod tests {
 
     #[test]
     pub fn small_even_parity() {
-        let iter = BitParityIter::new(true);
+        let iter = BitParityIter::<usize>::new(true);
         let v = iter.take(4).collect_vec();
 
         assert_eq!(v, [0x00, 0x3, 0x05, 0x06])
@@ -52,7 +60,7 @@ mod tests {
 
     #[test]
     pub fn small_odd_parity() {
-        let iter = BitParityIter::new(false);
+        let iter = BitParityIter::<usize>::new(false);
         let v = iter.take(4).collect_vec();
 
         assert_eq!(v, [0x01, 0x2, 0x04, 0x07])

09b2d60659de292ae5877d51a71fd71fbbf25b90
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2025-12-29 16:44:58 -0600

    initial impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,16 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, ItemEnum, Lit, Meta, NestedMeta};
+use syn::{parse_macro_input, ItemEnum, Meta};
 
-#[proc_macro_attribute]
-pub fn parity_enum(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args = parse_macro_input!(args as AttributeArgs);
-    let mut enum_item = parse_macro_input!(input as ItemEnum);
-
-    // Parse parity argument
-    let start = match args.as_slice() {
-        [NestedMeta::Meta(Meta::Path(path))] if path.is_ident("even") => 0,
-        [NestedMeta::Meta(Meta::Path(path))] if path.is_ident("odd") => 1,
-        _ => {
-            return syn::Error::new_spanned(quote! { #(#args),* }, "expected `even` or `odd`")
-                .to_compile_error()
-                .into();
+fn try_expand(args: Meta, mut enum_item: ItemEnum) -> syn::Result<TokenStream> {
+    let start = match args {
+        Meta::Path(path) if path.is_ident("even") => 0,
+        Meta::Path(path) if path.is_ident("odd") => 1,
+        p => {
+            return Err(syn::Error::new_spanned(
+                p.path(),
+                "expected `even` or `odd`",
+            ))
         }
     };
 
@@ -22,12 +18,10 @@ pub fn parity_enum(args: TokenStream, input: TokenStream) -> TokenStream {
 
     for variant in &mut enum_item.variants {
         if variant.discriminant.is_some() {
-            return syn::Error::new_spanned(
+            return Err(syn::Error::new_spanned(
                 &variant.ident,
                 "explicit discriminants are not allowed",
-            )
-            .to_compile_error()
-            .into();
+            ));
         }
 
         variant.discriminant = Some((syn::token::Eq::default(), syn::parse_quote!(#value)));
@@ -35,7 +29,21 @@ pub fn parity_enum(args: TokenStream, input: TokenStream) -> TokenStream {
         value += 2;
     }
 
-    TokenStream::from(quote! {
+    Ok(quote! {
         #enum_item
-    })
+    }
+    .into())
+}
+
+#[proc_macro_attribute]
+pub fn parity_enum(
+    args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let args = parse_macro_input!(args as Meta);
+    let enum_item = parse_macro_input!(input as ItemEnum);
+
+    try_expand(args, enum_item)
+        .map(Into::into)
+        .unwrap_or_else(|e| e.into_compile_error().into())
 }

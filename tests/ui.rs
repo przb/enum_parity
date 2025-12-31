18505d24aa8008de3f09f24db4d9409f7f27f772
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2025-12-30 22:20:55 -0600

    add test for cfg attr

--- tests/ui.rs
+++ tests/ui.rs
@@ -5,12 +5,12 @@ fn ui() {
     // valid usage
     t.pass("tests/ui/even_ok.rs");
     t.pass("tests/ui/odd_ok.rs");
+    t.pass("tests/ui/non_unit_enums.rs");
+    t.pass("tests/ui/cfg_attr.rs");
 
     // signed reprs
     t.pass("tests/ui/repr_u8.rs");
     t.pass("tests/ui/repr_i8.rs");
-
-    t.pass("tests/ui/non_unit_enums.rs");
 
     // invalid or unsupported usage
     t.compile_fail("tests/ui/repr_c.rs");
@@ -19,6 +19,7 @@ fn ui() {
     t.compile_fail("tests/ui/bad_arg_err.rs");
     t.compile_fail("tests/ui/overflow_err.rs");
 
+    // on other items
     t.compile_fail("tests/ui/on_struct.rs");
     t.compile_fail("tests/ui/on_mod.rs");
     t.compile_fail("tests/ui/on_trait.rs");
---
+++ tests/ui/cfg_attr.rs
@@ -0,0 +1,15 @@
+#[cfg_attr(target_arch = "x86_64", enum_parity::bit_parity(odd))]
+#[repr(u64)]
+enum OddEnum {
+    A,
+    B,
+    C,
+    D,
+}
+
+fn main() {
+    assert_eq!(OddEnum::A as u64, 1);
+    assert_eq!(OddEnum::B as u64, 2);
+    assert_eq!(OddEnum::C as u64, 4);
+    assert_eq!(OddEnum::D as u64, 7);
+}
